4ebd2f58c6c5eb401873f1a65d19083ffffc5042
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-01-19 10:12:13 +0200

    first commit

---
+++ minesweeper-tdd/src/external-types.d.ts
@@ -0,0 +1,16 @@
+declare module 'chai';
+declare module 'axios';
+declare module 'wix-axios-config';
+declare module 'wix-run-mode';
+declare module 'wix-bootstrap-testkit';
+declare module 'wix-config-emitter';
+declare module 'i18next-xhr-backend';
+declare module '*.scss';
+declare module '*.json';
+declare module 'enzyme';
+
+interface Window {
+  __STATICS_BASE_URL__: string;
+  __LOCALE__: string;
+  __BASEURL__: string;
+}
